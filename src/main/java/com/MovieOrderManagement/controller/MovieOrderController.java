a30c8fd20612759c5a13ddcdaadbfa85f4ab8cd6
Can Baran Engin <canbaranengin1@gmail.com>
2022-09-16 16:26:39 +0300

    Movie order methods have been updated

--- src/main/java/com/MovieOrderManagement/controller/MovieOrderController.java
+++ src/main/java/com/MovieOrderManagement/controller/MovieOrderController.java
@@ -2,6 +2,7 @@ package com.MovieOrderManagement.controller;
 
 
 import com.MovieOrderManagement.model.dto.MovieOrderDto;
+import com.MovieOrderManagement.model.dto.MovieOrderResponseDto;
 import com.MovieOrderManagement.model.entity.MovieOrder;
 import com.MovieOrderManagement.model.entity.Subscription;
 import com.MovieOrderManagement.service.MovieOrderService;
@@ -11,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Validated
@@ -28,16 +30,25 @@ public class MovieOrderController {
     }
 
     @PostMapping("movieOrder")
-    public ResponseEntity<MovieOrder> create(@RequestBody MovieOrderDto movieOrderDto){
+    public ResponseEntity<MovieOrderResponseDto> create(@RequestBody MovieOrderDto movieOrderDto){
         MovieOrder movieOrder =movieOrderService.create(movieOrderDto);
+        MovieOrderResponseDto movieOrderResponseDto= new MovieOrderResponseDto();
+        movieOrderResponseDto.setMovieName(movieOrder.getMovie().getTitle());
+        movieOrderResponseDto.setUserName(movieOrder.getUser().getUserName());
         //MovieOrderDto result = modelMapper.map(movieOrder,MovieOrderDto.class);
-        return ResponseEntity.ok(movieOrder);
+        return ResponseEntity.ok(movieOrderResponseDto);
 
     }
 
     @GetMapping("/movieorders")
-    public ResponseEntity<List<MovieOrder>> getAll(){
+    public ResponseEntity<List<MovieOrderResponseDto>> getAll(){
         List<MovieOrder> movieOrderList=movieOrderService.getAll();
-        return ResponseEntity.ok(movieOrderList);
+        List<MovieOrderResponseDto> movieOrderResponseDtoList = new ArrayList<>();
+        for (int i = 0; i < movieOrderList.size(); i++) {
+            movieOrderResponseDtoList.add(new MovieOrderResponseDto());
+            movieOrderResponseDtoList.get(i).setMovieName(movieOrderList.get(i).getMovie().getTitle());
+            movieOrderResponseDtoList.get(i).setUserName(movieOrderList.get(i).getUser().getUserName());
+        }
+        return ResponseEntity.ok(movieOrderResponseDtoList);
     }
 }
---
+++ src/main/java/com/MovieOrderManagement/model/dto/MovieOrderResponseDto.java
@@ -0,0 +1,9 @@
+package com.MovieOrderManagement.model.dto;
+
+import lombok.Data;
+
+@Data
+public class MovieOrderResponseDto {
+    private String userName;
+    private String movieName;
+}
